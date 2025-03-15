869e8d6f585326896a45d298ae4ea42bf29a5eaf
dntech9x <dntech9x@gmail.com>
2025-03-15 15:11:10 +0700

    def EAN13, download

--- docs/javascripts/script.js
+++ docs/javascripts/script.js
@@ -3,7 +3,8 @@ var defaultValues = {
     CODE128A : "EXAMPLE",
     CODE128B : "Example text",
     CODE128C : "12345678",
-    EAN13 : "1234567890128",
+    EAN13 : "893838600001",
+    // EAN13 : "1234567890128",
     EAN8 : "12345670",
     UPC : "123456789999",
     CODE39 : "EXAMPLE TEXT",
@@ -18,7 +19,20 @@ var defaultValues = {
 };
 
 $(document).ready(function(){
+    let downloadPngBtn = document.getElementById("download-png");
+    let downloadSvgBtn = document.getElementById("download-svg");
+
+    downloadPngBtn.removeEventListener("click", downloadPNG);
+    downloadSvgBtn.removeEventListener("click", downloadSVG);
+
+    downloadPngBtn.addEventListener("click", downloadPNG);
+    downloadSvgBtn.addEventListener("click", downloadSVG);
+
+    $("#barcodeType").val("EAN13");
+    $("#userInput").val(defaultValues["EAN13"]);
+
     $("#userInput").on('input',newBarcode);
+
     $("#barcodeType").change(function(){
         $("#userInput").val( defaultValues[$(this).val()] );
 
@@ -77,9 +91,12 @@ $(document).ready(function(){
 });
 
 var newBarcode = function() {
+    var code = $("#userInput").val();
+    if($("#barcodeType").val() == "EAN13" && code.length == 12)
+      code += calculateEAN13Checksum(code);
+
     //Convert to boolean
-    $("#barcode").JsBarcode(
-        $("#userInput").val(),
+    $("#barcode").JsBarcode(code,
         {
           "format": $("#barcodeType").val(),
           "background": $("#background-color").val(),
@@ -112,3 +129,60 @@ var newBarcode = function() {
     $("#bar-margin-display").text($("#bar-margin").val());
     $("#bar-text-margin-display").text($("#bar-text-margin").val());
 };
+
+function calculateEAN13Checksum(code) {
+    if (code.length !== 12) throw new Error("EAN-13 must have 12 digits before checksum");
+
+    let sumOdd = 0, sumEven = 0;
+
+    for (let i = 0; i < 12; i++) {
+        let num = parseInt(code[i], 10);
+        if (i % 2 === 0) sumOdd += num;
+        else sumEven += num;
+    }
+
+    let total = sumOdd + (sumEven * 3);
+    let checksum = (10 - (total % 10)) % 10;
+
+    return checksum;
+}
+
+function downloadPNG() {
+    let svg = document.getElementById("barcode");
+    let canvas = document.createElement("canvas");
+    let ctx = canvas.getContext("2d");
+    let img = new Image();
+
+    let svgData = new XMLSerializer().serializeToString(svg);
+    let svgBlob = new Blob([svgData], { type: "image/svg+xml;charset=utf-8" });
+    let url = URL.createObjectURL(svgBlob);
+
+    img.onload = function () {
+        canvas.width = img.width;
+        canvas.height = img.height;
+        ctx.drawImage(img, 0, 0);
+        URL.revokeObjectURL(url);
+        downloadImage(canvas.toDataURL("image/png"), "barcode.png");
+    };
+
+    img.src = url;
+}
+
+function downloadSVG() {
+    let svg = document.getElementById("barcode");
+    let svgData = new XMLSerializer().serializeToString(svg);
+    let blob = new Blob([svgData], { type: "image/svg+xml;charset=utf-8" });
+    let url = URL.createObjectURL(blob);
+
+    downloadImage(url, "barcode.svg");
+    URL.revokeObjectURL(url);
+}
+
+function downloadImage(dataUrl, filename) {
+    let link = document.createElement("a");
+    link.href = dataUrl;
+    link.download = filename;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
